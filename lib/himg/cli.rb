217d535fb0f324e8108cb6e6ece7b78ed704e49d
James Edwards-Jones <git@jamedjo.co.uk>
2025-07-23 02:24:34 +0100

    Adds disable_fetch/base_url to CLI

--- lib/himg/cli.rb
+++ lib/himg/cli.rb
@@ -10,7 +10,9 @@ module Himg
     option :height, type: :numeric, desc: "Sets the desired height of the rendered output.", default: 405
     option :truncate, type: :boolean, desc: "Keeps the image height fixed instead of expanding to include the full page.", default: true
     option :verbose, type: :boolean, desc: "Enables detailed logging for debugging and profiling.", default: false
+    option :disable_fetch, type: :boolean, desc: "Skip fetching file/http resources (stylesheets, images, fonts, etc)", default: false
     option :http_headers, desc: "HTTP Headers to use when fetching remote resource"
+    option :base_url, desc: "Base URL used to resolve relative URLs"
 
     long_desc <<-LONGDESC
       `himg screenshot` takes a path to an HTML file and will render a png image with the output.
