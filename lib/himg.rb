581accd460893f042aa9982611ad0bc20329d092
James Edwards-Jones <git@jamedjo.co.uk>
2025-07-31 03:45:57 +0100

    Allow config to be passed direct to `render himg:`

--- lib/himg.rb
+++ lib/himg.rb
@@ -17,7 +17,7 @@ end
 #
 # Converts HTML to an Image for a minimal subset of HTML and CSS
 module Himg
-  RENDER_OPTIONS = %i[width height truncate verbose].freeze
+  RENDER_OPTIONS = %i[width height truncate verbose base_url disable_fetch fetch_timeout gpu].freeze
   class Error < StandardError; end
 
   def self.render(html, width: 720, height: 405, truncate: true, verbose: false, base_url: nil, disable_fetch: false, fetch_timeout: 10, gpu: false)
